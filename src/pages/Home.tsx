51ca31fcb0b0f985a5ad65578d349ceecd46e755
Thitiporn Sukpartcharoen <osacn_thitiporn.sukpartcharoen@krungthai.com>
2023-03-26 01:53:10 +0700

    MockUp data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Treemap from "../charts/Treemap";
 import Hero from "../components/hero/hero";
 import Navbar from "../components/navbar/navbar";
 import WordCloud from "../components/wordcloud/wordcloud";
 import data from "../data";
 import Card from "../components/card/card";
+import Cluster from "../types/Cluster";
 type Props = {};
 
 const Home = (props: Props) => {
+  // Set useState Data
+  const [treeData, setTreeData] = useState<Cluster>();
+  useEffect(() => {
+    const fetchDatas = async () => {
+      const res = await fetch("http://localhost:8000/api/treemap");
+      const data = await res.json();
+      const tempData = {
+        name: 'Cluster',
+        children: data.treemap
+      }
+      console.log("Data ", data.treemap)
+      setTreeData(tempData);
+    };
+    fetchDatas();
+  }, []);
+
   return (
     <div className="bg-no-repeat min-h-screen w-full">
       <body className="bg-auto bg-no-repeat min-h-screen w-full ">
@@ -21,7 +38,9 @@ const Home = (props: Props) => {
             </div>
             <div className="grid grid-cols-1 gap-4 place-items-center pt-5">
               <WordCloud />
-              <Treemap data={data} height={400} width={600} />
+              {
+                treeData && <Treemap data={treeData} height={400} width={600} />
+              }
             </div>
             </div>
           </div>
