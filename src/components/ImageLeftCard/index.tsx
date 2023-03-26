b327b854aa25a2807289d99ebf40bd8b4d3e29b1
Thitiporn Sukpartcharoen <osacn_thitiporn.sukpartcharoen@krungthai.com>
2023-03-26 12:26:50 +0700

    Logo asset

--- src/components/ImageLeftCard/index.tsx
+++ src/components/ImageLeftCard/index.tsx
@@ -4,6 +4,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import PolicyLogo from "../../assets/partyLogo";
 
 interface Props {
   party: string;
@@ -18,7 +19,7 @@ export default function ImageLeftCard(props: Props) {
     <div className="w-full max-w-full flex shadow-md mb-10 border rounded-md">
       <div
         className="h-auto lg:h-auto w-64 flex-none bg-cover rounded-l text-center overflow-hidden"
-        style={{ backgroundImage: "url('/mock_card.jpg')" }}
+        style={{ backgroundImage: `url('/Partylogo/${PolicyLogo.indexOf(props.party)}.png')` , height: '100px', width: '100px'}}
         title="Woman holding a mug"
       ></div>
       <div className="rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
