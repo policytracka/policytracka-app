acbedf0f8c4c8d0f59244c6d65d9c800bb364afb
Thitiporn Sukpartcharoen <osacn_thitiporn.sukpartcharoen@krungthai.com>
2023-05-01 14:35:08 +0700

    Responseive nav bar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,19 +1,90 @@
 import React from "react";
 import Logo from "../../assets/Logo_white.png";
-import { useNavigate ,useParams} from 'react-router-dom'
+import { useNavigate, useParams } from "react-router-dom";
 type Props = {};
 
+const navContent = [
+  {
+    name: "Home",
+    id: "home_page",
+    path: "/",
+  },
+  {
+    name: "เปรียบเทียบนโยบาย",
+    id: "compare_policy_page",
+    path: "",
+  },
+  {
+    name: "ติดตามนโยบาย",
+    id: "follow_policy_page",
+    path: "",
+  },
+];
+
 const Navbar = (props: Props) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
   return (
-    <div className="flex 	justify-between text-[20px] text-white	font-bold	 items-center h-32 	w-[100vw] max-w-screen-2xl	">
-      <img src={Logo} className="w-[237px] ml-[73px]" onClick={()=>{navigate('/')}} />
-      <ul className="flex w-[550px] justify-evenly items-center mr-5">
-        <li className="">เปรียบเทียบนโยบาย</li>
-        <li className="">ติดตามนโยบาย</li>
-        <li>เกี่ยวกับ</li>
-      </ul>
-    </div>
+    <nav>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <img
+            src={Logo}
+            className="w-[237px] ml-[73px]"
+            alt="Flowbite Logo"
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+        </a>
+        ,
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+          onClick={() => {
+            setNavbarOpen(!navbarOpen);
+          }}
+        >
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div
+          className={
+            "w-full md:block md:w-auto" + (navbarOpen ? "" : " hidden")
+          }
+          id="navbar-default"
+        >
+          <ul className="flex flex-col p-4 md:p-0 mt-4 bg-transparent border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            {navContent.map((item) => (
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pl-3 pr-4 text-white rounded bg-transparent md:p-0"
+                  onClick={() => navigate(item.path)}
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
